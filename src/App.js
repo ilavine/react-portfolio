50145240e926d42ade0de067918ce4b4a34fd28d
unknown <iuliia.lavine@gmail.com>
2023-03-08 15:36:36 -0500

    add contact section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <Router>
---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Contact() {
+    return (
+
+        <div>
+            <div class="contact-form-wrapper d-flex justify-content-center">
+                <form action="#" class="contact-form">
+                    <h5 class="title">Contact Me</h5>
+                    <p class="description">Feel free to contact me.
+                    </p>
+                    <div>
+                        <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required/>
+                    </div>
+                    <div>
+                        <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required/>
+                    </div>
+                    <div>
+                        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required></textarea>
+                    </div>
+                    <div class="submit-button-wrapper">
+                        <input type="submit" value="Send"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    );
+}
+    
+    
+  
+  export default Contact;
