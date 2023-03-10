5b59c2f8d594ed58503381f9afeb65803bde6326
unknown <iuliia.lavine@gmail.com>
2023-03-10 15:18:48 -0500

    add contact function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,56 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Contact() {
-    return (
+  const [formData, setFormData] = useState({ subject: "", message: ""});
 
-        <div>
-            <div class="contact-form-wrapper d-flex justify-content-center">
-                <form action="#" class="contact-form">
-                    <h5 class="title">Contact Me</h5>
-                    <div>
-                        <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required/>
-                    </div>
-                    <div>
-                        <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required/>
-                    </div>
-                    <div>
-                        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required></textarea>
-                    </div>
-                    <div class="submit-button-wrapper">
-                        <input class="submit-button" type="submit" value="Send"/>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-    );
-}
-    
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
   
-  export default Contact;
+    const url = `mailto:yulia.lavine@gmail.com?subject=${formData.subject}&body=${formData.message}`;
+    window.open(url);
+    
+  };
+  return (
+    <div>
+      <div className="contact-form-wrapper d-flex justify-content-center">
+        <form action="#" className="contact-form" onSubmit={handleSubmit}>
+          <h5 className="title">Contact Me</h5>
+          <div>
+            <input
+              type="text"
+              className="form-control rounded border-white mb-3 form-input"
+              id="name"
+              placeholder="Subject"
+              required
+              value={formData.subject}
+              onChange={(e) => {
+                setFormData({ ...formData, subject: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <textarea
+              id="message"
+              className="form-control rounded border-white mb-3 form-text-area"
+              rows="5"
+              cols="30"
+              placeholder="Message"
+              required
+              value={formData.message}
+                onChange={(e) => {
+                  setFormData({ ...formData, message: e.target.value });
+                }}
+            ></textarea>
+          </div>
+          <div className="submit-button-wrapper">
+            <input className="submit-button" type="submit" value="Send" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
