487646134aa377f9630ccb0842ef4adbc776c56f
unknown <iuliia.lavine@gmail.com>
2023-04-14 15:25:05 -0400

    portfolio redesign

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HashRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
-import About from "./components/About/About";
+import About from "./pages/Home/About";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/ContactForm/Contact";
 import ProjectMap from "./components/Projects/ProjectMap";
@@ -12,12 +13,17 @@ function App() {
   return (
     <Router>
       <Header />
-      <Banner />
-      <About />
-      <Routes>
-      </Routes>
-      <ProjectMap />
-      <Contact />
+        <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/questionnaire" element={<Questionnaire />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/questionnaire" element={<Questionnaire/>} />
+              <Route path="/matches" element={<Matches/>} />
+              <Route path="/profile/:userId" element={<Profile />} />
+              <Route path="/about" element={<AboutMe />} />
+              <Route path="/about" element={<Directory />} />
+          </Routes>
       <Footer />
     </Router>
   );
